b7c6d11a1c528c2812411e997983b9537d753375
Hanan <hanan.ashrafi@lycee-faure.fr>
2026-03-10 17:00:12 +0100

    factory : ajout méthode vehicule selon besoins

--- factory/app/Factory/VehiculeFactory.php
+++ factory/app/Factory/VehiculeFactory.php
@@ -23,4 +23,28 @@ class VehiculeFactory
     {
         return new Bicycle(0, 'none');
     }
+
+    public static function makeVehicule(string $type): Vehicule
+    {
+        return match ($type) {
+            'car' => self::makeCar(),
+            'truck' => self::makeTruck(),
+            'bicycle' => self::makeBicycle(),
+            default => throw new \InvalidArgumentException("Type de véhicule inconnu : $type"),
+        };
+    }
+
+    public static function makeVehiculeFromNeeds(int $distance, int $weight): Vehicule
+    {
+        if ($weight > 200) {
+            return self::makeTruck();
+        }
+
+        if ($weight > 20 || $distance >= 20) {
+            return self::makeCar();
+        }
+
+        return self::makeBicycle();
+    }
+
 }
--- factory/public/index.php
+++ factory/public/index.php
@@ -13,4 +13,16 @@ echo 'Fuel type: ' . $truck->getFuelType() . "<br><br>";
 
 $bicycle = VehiculeFactory::makeBicycle();
 echo 'Cost per km: ' . $bicycle->getCostPerKm() . "<br>";
-echo 'Fuel type: ' . $bicycle->getFuelType() . "<br>";
+echo 'Fuel type: ' . $bicycle->getFuelType() . "<br><br>";
+
+$vehicule = VehiculeFactory::makeVehicule('truck');
+echo 'Type demandé : \'truck\' → coût/km = ' . $vehicule->getCostPerKm() . '€, carburant = ' . $vehicule->getFuelType() . "<br><br>";
+
+$v1 = VehiculeFactory::makeVehiculeFromNeeds(10, 5);
+echo 'Distance = 10km, Poids = 5kg → coût/km = ' . $v1->getCostPerKm() . '€, carburant = ' . $v1->getFuelType() . ' (vélo)<br>';
+
+$v2 = VehiculeFactory::makeVehiculeFromNeeds(30, 10);
+echo 'Distance = 30km, Poids = 10kg → coût/km = ' . $v2->getCostPerKm() . '€, carburant = ' . $v2->getFuelType() . ' (voiture)<br>';
+
+$v3 = VehiculeFactory::makeVehiculeFromNeeds(50, 300);
+echo 'Distance = 50km, Poids = 300kg → coût/km = ' . $v3->getCostPerKm() . '€, carburant = ' . $v3->getFuelType() . ' (camion)<br>';
