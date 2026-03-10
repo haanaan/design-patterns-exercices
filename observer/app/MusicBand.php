82c8584d31e770b73bb199281629090f33a93e63
Hanan <hanan.ashrafi@lycee-faure.fr>
2026-03-10 17:16:04 +0100

    observer : test observer et client fonctionnel

--- observer/app/MusicBand.php
+++ observer/app/MusicBand.php
@@ -2,8 +2,10 @@
 
 namespace App;
 
-class MusicBand 
+class MusicBand implements \SplSubject
 {
+    private array $observers = [];
+
     // Hors exercice mais notable:
     // Promotion du constructeur: https://www.php.net/manual/fr/language.oop5.decon.php#language.oop5.decon.constructor.promotion
     public function __construct(
@@ -14,15 +16,30 @@ class MusicBand
 
     public function addNewConcertDate(string $date, string $location):void
     {
-        $this->concert = [
+        $this->concerts[] = [
             'date' =>  $date,
             'location' => $location
         ];
+        $this->notify();
     }
 
-    public function attach(): void 
-    {}
+    public function attach(\SplObserver $observer): void 
+    {
+        $this->observers[] = $observer;
+    }
 
-    public function detach(): void 
-    {}
+    public function detach(\SplObserver $observer): void 
+    {
+        $key = array_search($observer, $this->observers, true);
+        if ($key !== false) {
+            unset($this->observers[$key]);
+        }
+    }
+
+    public function notify(): void
+    {
+        foreach ($this->observers as $observer) {
+            $observer->update($this);
+        }
+    }
 }
