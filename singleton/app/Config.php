7c9c94b7ce86d6fdc9e3bae83c3c079728a4fd86
Hanan <hanan.ashrafi@lycee-faure.fr>
2026-03-10 17:26:02 +0100

    singleton : classe config adapté avec settings et get key

--- singleton/app/Config.php
+++ singleton/app/Config.php
@@ -3,3 +3,33 @@
 # TODO: Créer une classe Config en Singleton
 
 namespace App;
+
+final class Config
+{
+    private static ?Config $instance = null;
+    private static array $settings = [];
+
+    private function __construct()
+    {
+        self::$settings = require __DIR__ . '/../config/config.php';
+    }
+
+    public static function getInstance(): Config
+    {
+        if (self::$instance === null) {
+            self::$instance = new Config();
+        }
+        return self::$instance;
+    }
+
+    public function get(string $key)
+    {
+        return self::$settings[$key] ?? null;
+    }
+
+    private function __clone() {}
+    public function __wakeup()
+    {
+        throw new \Exception("Cannot unserialize a singleton.");
+    }
+}
--- singleton/public/index.php
+++ singleton/public/index.php
@@ -1,7 +1,14 @@
 <?php
 require('../vendor/autoload.php');
 
+use App\Config;
 
-# TODO: Récuperer une instance de Config
-# Afficher une valeur contenu dans config.php
-# Récupérer une seconde instance de Config et vérifié que les deux instances sont identiques
+$config1 = Config::getInstance();
+
+echo "Debug : " . ($config1->get('debug') ? 'true' : 'false') . "<br>";
+echo "API Key : " . $config1->get('apiKey') . "<br>";
+echo "DB Host : " . $config1->get('db')['host'] . "<br>";
+
+$config2 = Config::getInstance();
+
+echo "\nLes deux instances sont identiques : " . ($config1 === $config2 ? 'oui' : 'non') . "<br>";
